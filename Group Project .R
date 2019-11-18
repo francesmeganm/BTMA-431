84724dc7a8b2ce1a1bf4c11f5dadcbdafed111c6
tessa22 <57731396+tessa22@users.noreply.github.com>
2019-11-18 16:04:06 -0700

    Update Group Project .R

--- Group Project .R
+++ Group Project .R
@@ -1,4 +1,15 @@
 # BTMA 431 Group Project 
 
-# hello world
+library(rvest)
+
+webpage <- read_html(https://www.samhsa.gov/data/sites/default/files/cbhsq-reports/NSDUHDetailedTabs2018R2/NSDUHDetTabsSect10pe2018.htm)
+tbls <- html_nodes(webpage, "table")
+head(tbls)
+tbls_ls <- webpage %>%
+  html_nodes("table")
+  .[13] %>%
+  html_table(fill = TRUE)
+str(tbls_ls)
+
+data.1 <- as.data.frame(tbls_ls)
 
