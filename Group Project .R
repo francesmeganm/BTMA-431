bf0f5fabdca1431223dba53f83abd3c4e174702d
francesmeganm <47461680+francesmeganm@users.noreply.github.com>
2019-11-24 15:10:03 -0700

    Update Group Project .R

--- Group Project .R
+++ Group Project .R
@@ -40,4 +40,45 @@ for (col in 1:ncol(table.10.7a)){
 
 View(table.10.7a)
 
+who <- read.csv("who_suicide_statistics.csv")
+who <- na.omit(who)
 
+# Suicides Per Capita 
+years <- unique(who$year)
+countries <- unique(who$country)
+suicides_country_year <- data.frame()
+for (c in 1:length(countries)){
+  country <- who %>%
+    filter(country == countries[c])
+  suicides_country_year[c, 1] <- toString(countries[c])
+  for (y in 1:length(years)){
+    current_year <- country %>%
+      filter(year == years[y])
+    total_pop <- sum(current_year$population)
+    total_suicides <- sum(current_year$suicides_no)
+    per_capita_suicides <- total_suicides/total_pop
+    suicides_country_year[c, y + 1] <- per_capita_suicides
+  }
+}
+
+colnames(suicides_country_year) <- c("Country", years)
+suicides_country_year[is.na(suicides_country_year)] = 0
+View(suicides_country_year)
+
+colnames(suicides_country_year)
+
+col_order <- c("Country", "1979", "1980", "1981","1982", "1983", "1984","1985", "1986", "1987","1988","1989", "1990",
+               "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999",
+               "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008",
+               "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016")
+
+suicides_country_year<- suicides_country_year[, col_order]
+View(suicides_country_year)
+
+for (i in 1:nrow(suicides_country_year)){
+  sum <- 0
+  for (c in 2:ncol(suicides_country_year)){
+    sum <- sum + suicides_country_year[i, c]
+  }
+  suicides_country_year$Sum[i] <- sum
+}
